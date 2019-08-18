fe45ce0b8c888562ffdc7343f15a4d5aa1393dac
jwcrazy90 <51671486+jwcrazy90@users.noreply.github.com>
2019-08-17 21:59:07 -0700

    This is a simple Nuxt.js sample

---
+++ pages/recipes/_id/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="single-recipe">
+        <h1>{{recipe.title}}</h1>
+        <div>
+            <img class="recipe-image" :src="recipe.thumbnail" :alt="recipe.previewText">
+        </div>
+        <p>{{recipe.description}}</p>
+    </section>
+</template>
+<script>
+export default {
+    asyncData(context){
+        return new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+                resolve({
+                    recipe:[{
+                        id:"1",
+                        title:"Delicious Pizza",
+                        previewText:"Awesome Pizza!",
+                        description : "this is really a delicious meal!",
+                        thumbnail:"https://sethlui.com/wp-content/uploads/2013/10/best-singapore-food-682.jpg"
+                    },
+                    {
+                        id:"2",
+                        title:"Sea food salad",
+                        previewText:"Very tasty indeed!",
+                        description : "this is really a delicious meal!",
+                        thumbnail:"https://assets3.thrillist.com/v1/image/2797371/size/tl-horizontal_main_2x.jpg"
+                    }].find(el => el.id === context.params.id)
+                })
+            },1500)
+        })
+    }
+}
+</script>
+<style scoped>
+    .single-recipe{
+        display:flex;
+        flex-flow:column;
+        justify-content:center;
+        align-items:center;
+        text-align:center;
+        padding:30px;
+    }
+    .recipe-image{
+        width:100%;
+    }
+</style>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class ="recipes">
+        <Recipe
+        v-for="recipe in recipes"
+        :key = "recipe.id"
+        :thumbnail ="recipe.thumbnail"
+        :title="recipe.title"
+        :id="recipe.id"
+        :previewText="recipe.previewText" />        
+    </section>
+</template>
+<script>
+import Recipe from '@/components/Recipe'
+export default {
+    components:{
+        Recipe
+    },
+    asyncData(){
+        return new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+                resolve({
+                    recipes:[{
+                        id:"1",
+                        title:"Delicious Pizza",
+                        previewText:"Awesome Pizza!",
+                        thumbnail:"https://sethlui.com/wp-content/uploads/2013/10/best-singapore-food-682.jpg"
+                    },
+                    {
+                        id:"2",
+                        title:"Sea food salad",
+                        previewText:"Very tasty indeed!",
+                        thumbnail:"https://assets3.thrillist.com/v1/image/2797371/size/tl-horizontal_main_2x.jpg"
+                    }]
+                })
+            },1500)
+        })
+    }
+}
+</script>
+<style scoped>
+    .recipes{
+        display:flex;
+        flex-flow:row wrap;
+        justify-content:center;
+        align-items:center;
+    }    
+</style>
